6d402669548ea14896e978ec2630319d53bb4d57
v_frlim <v_frlim@proximabeta.com>
2022-08-26 16:51:37 +0800

    views and table added

--- app.py
+++ app.py
@@ -4,26 +4,12 @@
 import os
 from flask import Flask, render_template, request, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Table, Column, Integer, String
+from sqlalchemy import Table, Column, Integer, String, text
 
-
-# basedir = os.path.abspath(os.path.dirname(__file__))
 app=Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:postgres@localhost/dict'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db=SQLAlchemy(app)
-
-
-# metadata_obj = MetaData()
-
-# user = Table(
-#     'user',
-#     metadata_obj,
-#     Column('user_id', Integer, primary_key=True),
-#     Column('user_name', String(16), nullable=False),
-#     Column('email_address', String(60)),
-#     Column('nickname', String(50), nullable=False)
-# )
 
 
 class Data(db.Model):
@@ -39,9 +25,25 @@ class Data(db.Model):
         self.eng_=eng_
         self.pinyin_=pinyin_
     
+from app import db, Data
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    words = Data.query.all()
+    return render_template("index.html", content=words)
+
+# words = Data.query.all()
+# for word in words:
+    # print(word.pinyin_)
+    # print(word.chi_)
+    # print(word.eng_)
+
+# sorted = Data.query.order_by().all()
+# words = Data.query.all()
+# for word in words:
+#     print(Data.query(pinyin_).order_by().all())
+    # print(word.chi_)
+    # print(word.eng_)
 
 @app.route("/success", methods=['POST'])
 def success():
@@ -49,11 +51,6 @@ def success():
         chi=request.form["chi"]
         eng=request.form["eng"]
         pinyin=request.form["pinyin"]
-        # file=request.files["file"]
-        # file.save(secure_filename("uploaded"+file.filename))
-        # with open("uploaded"+file.filename,"a") as f:
-        # f.write("This was added later!")
-        # print(type(file))
         
         # --- TEST WRITING CONDITIONAL , step by step
         # print(chi,eng,pinyin)
@@ -61,10 +58,6 @@ def success():
         # print(db.session.query(Data).filter(Data.chi_==chi))
         # print(db.session.query(Data).filter(Data.chi_==chi).count())
         
-        # for item in range:
-        #     range = db.session.query(Data).filter(Data.chi_==chi)
-        #     print(item)
-            
         # check if unique,
         if db.session.query(Data).filter(Data.chi_==chi).count() == 0:
             data=Data(chi,eng,pinyin)
@@ -73,24 +66,6 @@ def success():
             return render_template("success.html")
     return render_template("index.html", text="word already exists")
 
-
-result_set = db.session.query("SELECT * FROM data")  
-for r in result_set:  
-    print(r)
-
-
-# @app.route("/pinyin")
-# , methods=['GET']
-# def pinyin():
-#     # data=Data(pinyin)
-#     data = Data.query.filter_by(Data.pinyin_==pinyin).all()
-#     return f'{data.pinyin_}'
-    
-# @app.route("/english.html", methods=['GET'])
-    # def english():
-
-
-# print(db.session.query(Data).filter(Data.chi_==chi))
 
 
 if __name__ == '__main__':
